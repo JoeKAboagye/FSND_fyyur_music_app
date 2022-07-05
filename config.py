fc18e1c284caa713eaaedf2698f579a75eff7179
JoeKAboagye <josephkwabenaaboagye@gmail.com>
2022-07-05 04:29:07 +0000

    App made secure

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,5 +11,6 @@ DEBUG = True
 
 # Connect to the database
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Joekab$166114@localhost:5432/artistbooker'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(
+    os.getenv("DB_USER"), os.getenv("DB_USER_PASSWORD"), os.getenv("DB_HOST"), os.getenv("DB_NAME"))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
